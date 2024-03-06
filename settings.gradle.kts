298c4a60076809106c0948f66efe5595d4a63ade
Leo Hoerdemann <leo.h.trip@gmail.com>
2024-03-06 17:44:40 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CSE 476 Project App"
+include(":app")
